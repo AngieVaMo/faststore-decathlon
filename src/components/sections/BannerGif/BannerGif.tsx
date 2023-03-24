a0f0722753b5432ec3bdb416a153036c20e12883
AngieVaMo <anvamopa2@gmail.com>
2023-03-24 16:36:02 -0500

    (feat/plp): plp styles

--- src/components/sections/BannerGif/BannerGif.tsx
+++ src/components/sections/BannerGif/BannerGif.tsx
@@ -24,27 +24,25 @@ function BannerGif({
   children,
 }: Props) {
   return (
-    <div>
-      <Banner className={styles.fsBannerGif} data-fs-banner-gif={classBanner}>
-        <div data-fs-banner-gif-img-link>
-          <BannerImage data-fs-banner-gif-image>
-            <img src={url.default} alt={imgDescription} />
-          </BannerImage>
-          <BannerLink data-fs-banner-gif-link>
-            <BannerButton
-              data-fs-banner-gif-link-button
-              //  data-fs-banner-gif={classBanner}
-              variant="primary"
-              href={href}
-              icon={icon}
-            >
-              {btnText}
-            </BannerButton>
-          </BannerLink>
-        </div>
-        {children}
-      </Banner>
-    </div>
+    <Banner className={styles.fsBannerGif} data-fs-banner-gif={classBanner}>
+      <div data-fs-banner-gif-img-link>
+        <BannerImage data-fs-banner-gif-image>
+          <img src={url.default} alt={imgDescription} />
+        </BannerImage>
+        <BannerLink data-fs-banner-gif-link>
+          <BannerButton
+            data-fs-banner-gif-link-button
+            //  data-fs-banner-gif={classBanner}
+            variant="primary"
+            href={href}
+            icon={icon}
+          >
+            {btnText}
+          </BannerButton>
+        </BannerLink>
+      </div>
+      {children}
+    </Banner>
   )
 }
 
